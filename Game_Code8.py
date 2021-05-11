4aef14f74d5c56184e45b825c733007e7f744351
Debajyoti_Roy_Chowdhury <63442163+debajyotiroyc@users.noreply.github.com>
2021-05-11 15:58:37 +0530

    Add files via upload

---
+++ Game_Code8.py
@@ -0,0 +1,82 @@
+import pygame,random,math
+pygame.init()
+
+screen=pygame.display.set_mode((800,800))
+pygame.display.set_caption("Enemy")
+
+icon=pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
+
+play1=pygame.image.load('spaceship.png')
+enemy1=pygame.image.load('enemy.png')
+bullet1=pygame.image.load('bullet.png')
+bullX=0
+bullY=650
+bull_vel=1
+bull_state="ready"
+x1=random.randint(0,735)
+y1=random.randint(0,50)
+x,y=350,650
+vel,vel1=0,0.5
+def player(x,y):
+    screen.blit(play1,(x,y))
+def enemyplayer(x1,y1):
+    screen.blit(enemy1, (x1, y1))
+def bullet_shot(x,y):
+    global bull_state
+    bull_state="fire"
+    screen.blit(bullet1,(x+14,y+10))
+def isCollision(x,y,x1,y1):
+    m = math.sqrt(math.pow((x - x1), 2) + math.pow((y - y1), 2))
+    if m < 27:
+        return True
+    else:
+        return False
+run=True
+while run:
+    screen.fill((0,0,0))
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            run=False
+        if event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_RIGHT:
+                vel+=0.5
+            if event.key==pygame.K_LEFT:
+                vel-=0.5
+            if event.key==pygame.K_SPACE:
+                if bull_state=="ready":
+                    bullX=x
+                    bullet_shot(bullX,bullY)
+        if event.type==pygame.KEYUP:
+            if event.key == pygame.K_RIGHT or event.key==pygame.K_LEFT:
+                vel=0
+    x+=vel
+    x1+=vel1
+    if x<0:
+        x=0
+    if x>736:
+        x=736
+    if x1<0:
+        x1=0
+        vel1=0.7
+        y1+=20
+    if x1>736:
+        x1=736
+        vel1=-0.7
+        y1+=20
+    if bullY<0:
+        bullY=650
+        bull_state="ready"
+    if bull_state == "fire":
+        bullet_shot(bullX,bullY)
+        bullY-=bull_vel
+    if isCollision(x1,y1,bullX,bullY):
+        x1 = random.randint(0, 735)
+        y1 = random.randint(0, 50)
+        bull_state="ready"
+        bullY = 650
+    player(x,y)
+    enemyplayer(x1,y1)
+    pygame.display.update()
+
+pygame.quit()
